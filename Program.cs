b9268f26768fc6d29f64e32a920b997f1d79a028
Kohei Dunnet <kdunnet1@my.bcit.ca>
2025-01-24 14:25:33 -0800

    fixed

--- Program.cs
+++ Program.cs
@@ -36,5 +36,12 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+    
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();    
+    context.Database.Migrate();
+}
 
 app.Run();
